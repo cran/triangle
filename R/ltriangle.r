309e81a9a1e072ac99ea487dd05f6d6d42ee2f43
Rob Carnell <bertcarnell@gmail.com>
2019-02-14 22:22:15 +0000

    version 0.12

--- R/ltriangle.r
+++ R/ltriangle.r
@@ -1,34 +1,60 @@
-################################################################################
-#
-# Program:   ltriangle.R
-# Purpose:   To draw from the log10 triangle distribution
-# Author:    Rob Carnell
-# Date:      June 06
-#
-# Variables
-#   used the same naming conventions as other R distributions (r,q,p,d)
-#   n = number of values to return
-#   a = left triangle endpoint,
-#   b = right triangle endpoint
-#   c = distribution mode
-#
-################################################################################
+# Copyright 2018 Rob Carnell
 
-rltriangle <- function(n=1, a=1, b=100, c=10^((log10(a)+log10(b))/2), logbase=10)
+#' The Log-Triangle Distribution
+#'
+#' @description These functions provide information about the triangle distribution on the
+#' logarithmic interval from \code{a} to \code{b} with a maximum at \code{c}.  \code{dltriangle}
+#' gives the density, \code{pltriangle} gives the distribution function,
+#' \code{qltriangle} gives the quantile function, and \code{rltriangle} generates
+#' \code{n} random deviates.
+#'
+#' @details  All probabilities are lower tailed probabilties.  \code{a},
+#' \code{b}, and \code{c} may be appropriate length vectors except in the
+#' case of \code{rtriangle}.
+#'
+#' @param x,q vector of quantiles.
+#' @param a lower limit of the distribution.
+#' @param b upper limit of the distribution.
+#' @param c mode of the distribution.
+#' @param p vector of probabilities.
+#' @param n number of observations. If \code{length(n) > 1}, the length is taken to be the number required.
+#' @param logbase the base of the logarithmic scale to use (default to 10)
+#'
+#' @return   \code{dltriangle} gives the density, \code{pltriangle} gives the
+#' distribution function, \code{qltriangle} gives the quantile function, and
+#' \code{rltraingle} generates random deviates.  Invalid arguments will
+#' result in return value \code{NaN} or \code{NA}.
+#' @references
+#' Becker, R. A., Chambers, J. M. and Wilks, A. R. (1988) \emph{The New S Language}.  Wadsworth \& Brooks/Cole.
+#' @seealso
+#' \code{\link{.Random.seed}} about random number generation,
+#' \code{\link{runif}}, etc for other distributions.
+#' @keywords distribution
+#'
+#' @name ltriangle
+#' @importFrom stats runif
+#' @export
+#'
+#' @examples
+#' tri <- rltriangle(100000, 1, 100, 10)
+#' hist(log10(tri), breaks=100, main="Triangle Distribution", xlab="x")
+#' dltriangle(10, 1, 100, 10) # 2/(log10(b)-log10(a)) = 1
+#' qltriangle(pltriangle(10)) # 10
+rltriangle <- function(n=1, a=1, b=100, c=10^((log10(a) + log10(b))/2), logbase=10)
 {
   stopifnot(length(n) == 1)
-  if(n < 1 | is.na(n)) stop(paste("invalid argument: n =", n))
+  if (n < 1 | is.na(n)) stop(paste("invalid argument: n =", n))
   n <- floor(n)
-  if(any(is.na(c(a,b,c)))) return(rep(NaN, times=n)) # to match behavior of runif
-  if(any(a > c | b < c)) return(rep(NaN, times=n)) # to match behavior of runif
-  if(any(is.infinite(c(a, b, c)))) return(rep(NaN, times=n))
-  if(any(c(a,b,c) == 0)) return(rep(-Inf, times=n))
-  if(any(c(a,b,c) < 0)) return(rep(NaN, times=n))
-  
+  if (any(is.na(c(a,b,c)))) return(rep(NaN, times = n)) # to match behavior of runif
+  if (any(a > c | b < c)) return(rep(NaN, times = n)) # to match behavior of runif
+  if (any(is.infinite(c(a, b, c)))) return(rep(NaN, times = n))
+  if (any(c(a,b,c) == 0)) return(rep(-Inf, times = n))
+  if (any(c(a,b,c) < 0)) return(rep(NaN, times = n))
+
   lp <- runif(n)
 
   stopifnot(length(logbase) == 1)
-  if (logbase==10)
+  if (logbase == 10)
   {
     la <- log10(a)
     lb <- log10(b)
@@ -39,8 +65,8 @@ rltriangle <- function(n=1, a=1, b=100, c=10^((log10(a)+log10(b))/2), logbase=10
     lb <- log(b)/log(logbase)
     lc <- log(c)/log(logbase)
   }
-  
-  if(a != c)
+
+  if (a != c)
   {
     # if a = c then i is always true
     i <- which((la + sqrt(lp * (lb - la)*(lc - la))) <= lc)
@@ -51,24 +77,26 @@ rltriangle <- function(n=1, a=1, b=100, c=10^((log10(a)+log10(b))/2), logbase=10
     j <- which((lb - sqrt((1 - lp) * (lb - la) * (lb - lc))) >= lc)
   }
 
-  if(length(i) != 0)
+  if (length(i) != 0)
     lp[i] <- la + sqrt(lp[i] * (lb - la) * (lc - la))
-  if(length(j) != 0)
+  if (length(j) != 0)
     lp[j] <- lb - sqrt((1 - lp[j]) * (lb - la) * (lb - lc))
-    
+
   p <- logbase^lp
 
   return(p)
 }
 
-dltriangle <- function(x, a=1, b=100, c=10^((log10(a)+log10(b))/2), logbase=10) {
+#' @rdname ltriangle
+#' @export
+dltriangle <- function(x, a=1, b=100, c=10^((log10(a) + log10(b))/2), logbase=10) {
   x1 <- length(x)
   a1 <- length(a)
   b1 <- length(b)
   c1 <- length(c)
 
   stopifnot(length(logbase) == 1)
-  if (logbase==10)
+  if (logbase == 10)
   {
     la <- log10(a)
     lb <- log10(b)
@@ -83,36 +111,36 @@ dltriangle <- function(x, a=1, b=100, c=10^((log10(a)+log10(b))/2), logbase=10) 
   }
 
   dTest <- function(X){
-    if(any(is.na(X)))
+    if (any(is.na(X)))
     {
       # is.na is TRUE for NA, NaN, and FALSE
-      if(any(is.nan(X))) return(NaN) # to conform to qunif
+      if (any(is.nan(X))) return(NaN) # to conform to qunif
       else return(NA) # to conform to qunif
-    } else if(X[2] > X[4] | X[3] < X[4] | (X[1]==X[2] & X[2]==X[4]))
+    } else if (X[2] > X[4] | X[3] < X[4] | (X[1] == X[2] & X[2] == X[4]))
     {
       warning("values required to be  a <= c <= b (at least one strict inequality)")
       return(NaN) # to conform to behavior of qunif
-    } else if(any(is.infinite(X[2:4])))
+    } else if (any(is.infinite(X[2:4])))
     {
       return(NaN)
-    } else if(X[1] <= X[2])
+    } else if (X[1] <= X[2])
     {
       return(0)
-    } else if(X[2] != X[4] & X[1] < X[4])
+    } else if (X[2] != X[4] & X[1] < X[4])
     {
       return(2*(X[1] - X[2]) / (X[3] - X[2]) / (X[4] - X[2]))
-    } else if(X[4] != X[3] & X[1] >= X[4] & X[1] < X[3])
+    } else if (X[4] != X[3] & X[1] >= X[4] & X[1] < X[3])
     {
       return(2*(X[3] - X[1]) / (X[3] - X[2]) / (X[3] - X[4]))
-    } else if(X[1] >= X[3]){
+    } else if (X[1] >= X[3]) {
       return(0)
     }
   }
 
   k <- max(x1, a1, b1, c1)
-  if(k==1) return(dTest(c(lx, la, lb, lc)))
+  if (k == 1) return(dTest(c(lx, la, lb, lc)))
 
-  params <- matrix(nrow=k, ncol=4)
+  params <- matrix(nrow = k, ncol = 4)
   tryCatch(
   {
     params[,1] <- lx
@@ -121,13 +149,15 @@ dltriangle <- function(x, a=1, b=100, c=10^((log10(a)+log10(b))/2), logbase=10) 
     params[,4] <- lc
   }, error = function(X) {
     stop(paste(" -- Argument Lengths: length of x = ", x1,
-                ", a = ", a1, ", b = ", b1, ", c = ", c1, " -- ", X, sep=""))
+                ", a = ", a1, ", b = ", b1, ", c = ", c1, " -- ", X, sep = ""))
   })
 
   return(apply(params, 1, dTest))
 }
 
-pltriangle <- function(q, a=1, b=100, c=10^((log10(a)+log10(b))/2), logbase=10)
+#' @rdname ltriangle
+#' @export
+pltriangle <- function(q, a=1, b=100, c=10^((log10(a) + log10(b))/2), logbase=10)
 {
   q1 <- length(q)
   a1 <- length(a)
@@ -135,7 +165,7 @@ pltriangle <- function(q, a=1, b=100, c=10^((log10(a)+log10(b))/2), logbase=10)
   c1 <- length(c)
 
   stopifnot(length(logbase) == 1)
-  if (logbase==10)
+  if (logbase == 10)
   {
     la <- log10(a)
     lb <- log10(b)
@@ -151,35 +181,35 @@ pltriangle <- function(q, a=1, b=100, c=10^((log10(a)+log10(b))/2), logbase=10)
 
   pTest <- function(X)
   {
-    if(any(is.na(X))){ # is.na is TRUE for NA, NaN, and FALSE
-      if(any(is.nan(X))) return(NaN) # to conform to qunif
+    if (any(is.na(X))) { # is.na is TRUE for NA, NaN, and FALSE
+      if (any(is.nan(X))) return(NaN) # to conform to qunif
       else return(NA) # to conform to qunif
-    } else if(X[2] > X[4] | X[3] < X[4] | (X[1]==X[2] & X[2]==X[4]))
+    } else if (X[2] > X[4] | X[3] < X[4] | (X[1] == X[2] & X[2] == X[4]))
     {
       warning("values required to be  a <= c <= b (at least one strict inequality)")
       return(NaN) # to conform to behavior of qunif
-    } else if(any(is.infinite(X[2:4])))
+    } else if (any(is.infinite(X[2:4])))
     {
       return(NaN)
-    } else if(X[1] <= X[2])
+    } else if (X[1] <= X[2])
     {
       return(0)
-    } else if(X[2] != X[4] & X[1] < X[4])
+    } else if (X[2] != X[4] & X[1] < X[4])
     {
       return((X[1] - X[2])^2 / (X[3] - X[2]) / (X[4] - X[2]))
-    } else if(X[4] != X[3] & X[1] >= X[4] & X[1] < X[3])
+    } else if (X[4] != X[3] & X[1] >= X[4] & X[1] < X[3])
     {
       return(1 - (X[3] - X[1])^2 / (X[3] - X[2]) / (X[3] - X[4]))
-    } else if(X[1] >= X[3])
+    } else if (X[1] >= X[3])
     {
       return(1)
     }
   }
 
   k <- max(q1, a1, b1, c1)
-  if(k==1) return(pTest(c(lq, la, lb, lc)))
+  if (k == 1) return(pTest(c(lq, la, lb, lc)))
 
-  params <- matrix(nrow=k, ncol=4)
+  params <- matrix(nrow = k, ncol = 4)
   tryCatch(
   {
     params[,1] <- lq
@@ -188,13 +218,15 @@ pltriangle <- function(q, a=1, b=100, c=10^((log10(a)+log10(b))/2), logbase=10)
     params[,4] <- lc
   }, error = function(X) {
     stop(paste(" -- Argument Lengths: length of q = ", q1,
-                ", a = ", a1, ", b = ", b1, ", c = ", c1, " -- ", X, sep=""))
+                ", a = ", a1, ", b = ", b1, ", c = ", c1, " -- ", X, sep = ""))
   })
 
   return(apply(params, 1, pTest))
 }
 
-qltriangle <- function(p, a=1, b=100, c=10^((log10(a)+log10(b))/2), logbase=10)
+#' @rdname ltriangle
+#' @export
+qltriangle <- function(p, a=1, b=100, c=10^((log10(a) + log10(b))/2), logbase=10)
 {
   p1 <- length(p)
   a1 <- length(a)
@@ -202,7 +234,7 @@ qltriangle <- function(p, a=1, b=100, c=10^((log10(a)+log10(b))/2), logbase=10)
   c1 <- length(c)
 
   stopifnot(length(logbase) == 1)
-  if (logbase==10)
+  if (logbase == 10)
   {
     la <- log10(a)
     lb <- log10(b)
@@ -217,39 +249,42 @@ qltriangle <- function(p, a=1, b=100, c=10^((log10(a)+log10(b))/2), logbase=10)
   qTest <- function(X)
   {
     # X = c(p, a, b, c)
-    if(any(is.na(X))){ # is.na is TRUE for NA, NaN, and FALSE
-      if(any(is.nan(X))) return(NaN) # to conform to qunif
+    if (any(is.na(X))) { # is.na is TRUE for NA, NaN, and FALSE
+      if (any(is.nan(X))) return(NaN) # to conform to qunif
       else return(NA) # to conform to qunif
-    } else if(X[2] > X[4] | X[3] < X[4])
+    } else if (X[2] > X[4] | X[3] < X[4])
     {
       warning("values required to be  a <= c <= b (at least one strict inequality)")
       return(NaN) # to conform to behavior of qunif
-    } else if(X[1] < 0 | X[1] > 1)
+    } else if (X[1] < 0 | X[1] > 1)
     {
       warning("at least one p is outside [0,1]")
       return(NaN) # to conform to behavior of qunif
-    } else if(any(is.infinite(X)))
+    } else if (any(is.infinite(X)))
     {
       return(NaN)
-    } else if((X[2] != X[4] &&
-               (X[2] + sqrt(X[1]*(X[3]-X[2])*(X[4]-X[2]))) <= X[4]) |
+    } else if ((X[2] != X[4] &&
+               (X[2] + sqrt(X[1]*(X[3] - X[2])*(X[4] - X[2]))) <= X[4]) |
               (X[2] == X[4] &&
-               (X[2] + sqrt(X[1]*(X[3]-X[2])*(X[4]-X[2]))) < X[4]))
+               (X[2] + sqrt(X[1]*(X[3] - X[2])*(X[4] - X[2]))) < X[4]))
     {
-      return(logbase^(X[2] + sqrt(X[1]*(X[3]-X[2])*(X[4]-X[2]))))
-    } else if((X[2] != X[4] &&
-               (X[3] - sqrt((1-X[1])*(X[3]-X[2])*(X[3]-X[4]))) > X[4]) |
+      return(logbase^(X[2] + sqrt(X[1]*(X[3] - X[2])*(X[4] - X[2]))))
+    } else if ((X[2] != X[4] &&
+               (X[3] - sqrt((1 - X[1])*(X[3] - X[2])*(X[3] - X[4]))) > X[4]) |
               (X[2] == X[4] &&
-               (X[3] - sqrt((1-X[1])*(X[3]-X[2])*(X[3]-X[4]))) >= X[4]))
+               (X[3] - sqrt((1 - X[1])*(X[3] - X[2])*(X[3] - X[4]))) >= X[4]))
     {
-      return(logbase^(X[3] - sqrt((1-X[1])*(X[3]-X[2])*(X[3]-X[4]))))
+      return(logbase^(X[3] - sqrt((1 - X[1])*(X[3] - X[2])*(X[3] - X[4]))))
+    } else if (X[2] != X[4] && (X[2] + sqrt(X[1]*(X[3] - X[2])*(X[4] - X[2]))) - X[4] <= (X[3] - X[2])*.Machine$double.eps)
+    {
+      return(X[4])
     } else stop("Unexpected Result")
   }
 
   k <- max(p1, a1, b1, c1)
-  if(k==1) return(qTest(c(p,la,lb,lc)))
+  if (k == 1) return(qTest(c(p,la,lb,lc)))
 
-  params <- matrix(nrow=k, ncol=4)
+  params <- matrix(nrow = k, ncol = 4)
   tryCatch(
   {
     params[,1] <- p
@@ -258,9 +293,8 @@ qltriangle <- function(p, a=1, b=100, c=10^((log10(a)+log10(b))/2), logbase=10)
     params[,4] <- lc
   }, error = function(X) {
     stop(paste(" -- Argument Lengths: length of p = ", p1,
-                ", a = ", a1, ", b = ", b1, ", c = ", c1, " -- ", X, sep=""))
+                ", a = ", a1, ", b = ", b1, ", c = ", c1, " -- ", X, sep = ""))
   })
 
   return(apply(params, 1, qTest))
 }
-
---
+++ tests/testthat/test-ltriangle.r
@@ -0,0 +1,112 @@
+context("test-ltriangle")
+
+test_that("rltriangle",
+{
+  expect_true(max(range(rltriangle(100))) <= 100)
+  expect_true(min(range(rltriangle(100))) >= 1)
+  expect_true(length(rltriangle(20.3)) == 20)
+
+  expect_error(rltriangle(-5))
+  expect_equal(rltriangle(2, NaN, 3, 1), c(NaN, NaN))
+  expect_equal(rltriangle(1, 3, NA, 4), NaN)
+  expect_error(rltriangle(NA))
+
+  test <- rltriangle(1, 2, 5, 2 + 10^-6)
+  expect_true(test > 2 & test < 5)
+  test <- rltriangle(1, 2, 5, 2 + 10^-9)
+  expect_true(test > 2 & test < 5)
+  test <- rltriangle(1, 2, 5, 2 + 10^-12)
+  expect_true(test > 2 & test < 5)
+  expect_true(!is.nan(rltriangle(1, 2, 5, 2 + 10^-18)))
+
+  expect_true(!all(1 == rltriangle(100, 1, 100, 1)))
+  expect_true(!any(is.nan(rltriangle(100, 10, 100, 10))))
+  expect_true(all(!is.finite(rltriangle(10, -1, 0, -1))))
+  expect_true(all(is.na(rltriangle(10, -5, -2, -2))))
+  expect_true(all(1 == rltriangle(10,1,1,1)))
+  expect_true(all(is.na(rltriangle(10,-1,-1,-1))))
+
+  expect_true(all(is.nan(rltriangle(10, 5, 6, 4))))
+  expect_true(all(is.nan(rltriangle(10, 5, 6, 7))))
+  expect_true(all(is.nan(rltriangle(10, 5, Inf, 7))))
+
+  expect_true(all(rltriangle(3, 1, 100, 10, logbase = 7) >= 1))
+})
+
+test_that("dltriangle",
+{
+  expect_equal(dltriangle(1, 1, 3, 2), 0)
+  expect_equal(dltriangle(3, 1, 3, 2), 0)
+  expect_equal(dltriangle(10^0.5, 1, 10, 10^0.5), 2)
+  expect_warning(expect_equal(dltriangle(0, 0, 0, 0), NaN))
+  expect_warning(expect_equal(dltriangle(.5, 0, 1, 2), NaN))
+  expect_warning(expect_equal(dltriangle(.5, 1, 2, .5), NaN))
+  expect_equal(dltriangle(c(1, 3, 10^.5), c(1, 1, 1), c(3, 3, 10),
+                                c(2, 2, 10^.5)), c(0, 0, 2))
+
+  expect_equal(dltriangle(7, 1, 49, 7, logbase = 7), 1)
+
+  expect_true(is.na(dltriangle(.5, 0, NA, 3)))
+  expect_true(is.nan(dltriangle(.5, 0, 5, NaN)))
+  expect_true(is.nan(dltriangle(.5, 0, Inf, 3)))
+
+  expect_error(dltriangle(c(1,2,3), c(1,2,3), c(4,5), c(1,2,3)))
+
+  expect_equal(0.5, dltriangle(10^0.5, 1, 100, 10))
+})
+
+test_that("ptriangle",
+{
+  expect_equal(pltriangle(1, 1, 3, 2), 0)
+  expect_equal(pltriangle(3, 1, 3, 2), 1)
+  expect_equal(pltriangle(10^.5, 1, 10, 10^.5), .5)
+  expect_warning(expect_equal(pltriangle(0, 0, 0, 0), NaN))
+  expect_warning(expect_equal(pltriangle(.5, 0, 1, 2), NaN))
+  expect_warning(expect_equal(pltriangle(.5, 1, 2, .5), NaN))
+  expect_equal(pltriangle(c(1, 3, 10^.5), c(1, 1, 1), c(3, 3, 10),
+                                c(2, 2, 10^.5)), c(0, 1, .5))
+
+  expect_equal(pltriangle(7, 1, 49, 7, logbase = 7), 0.5)
+
+  expect_true(is.na(pltriangle(1, 0, NA, 3)))
+  expect_true(is.nan(pltriangle(1, 0, 5, NaN)))
+  expect_true(is.nan(pltriangle(1, 0, Inf, 3)))
+
+  expect_error(pltriangle(c(1,2,3), c(1,2,3), c(4,5), c(1,2,3)))
+})
+
+test_that("qltriangle",
+{
+  expect_equal(qltriangle(0, 1, 3, 2), 1)
+  expect_equal(qltriangle(0, 2, 3, 2), 2)
+  expect_equal(qltriangle(1, 1, 3, 2), 3)
+  expect_equal(qltriangle(.5, 1, 10, 10^.5), 10^.5)
+  expect_equal(qltriangle(0, 1, 1, 1), 1)
+  expect_warning(expect_equal(qltriangle(-1, 0, 1, .5), NaN))
+  expect_warning(expect_equal(qltriangle(2, 0, 1, .5), NaN))
+  expect_warning(expect_equal(qltriangle(.5, 0, 1, 2), NaN))
+  expect_warning(expect_equal(qltriangle(.5, 1, 2, .5), NaN))
+  expect_equal(qltriangle(.5, NA, 2, .5), NA)
+  expect_equal(qltriangle(c(0, 0, 1, .5), c(1, 2, 1, 1),
+                                c(3, 3, 3, 10), c(2, 2, 2, 10^.5)),
+                     c(1, 2, 3, 10^.5))
+  expect_error(qltriangle(c(0, 0, 0, 0, 0), c(0, 0), 1, .5))
+  expect_equal(qltriangle(.5, NaN, 2, 1), NaN)
+  expect_equal(qltriangle(.5, 0, Inf, 1), NaN)
+  expect_equal(qltriangle(.5, 0, Inf, Inf), NaN)
+
+  expect_equal(qltriangle(pltriangle(15, 1, 100, 10), 1, 100, 10), 15)
+  expect_equal(qltriangle(pltriangle(3, 2, 5, 5), 2, 5, 5), 3)
+  expect_equal(qltriangle(pltriangle(5, 2, 5, 5), 2, 5, 5), 5)
+
+  expect_equal(qltriangle(0.5, 1, 49, 7, logbase = 7), 7)
+
+  expect_true(is.na(qltriangle(0.5, 0, NA, 3)))
+  expect_true(is.nan(qltriangle(0.5, 0, 5, NaN)))
+  expect_true(is.nan(qltriangle(0.5, 0, Inf, 3)))
+
+  expect_error(qltriangle(c(.1,.2,.3), c(1,2,3), c(4,5), c(1,2,3)))
+
+  # numerical stability issue
+  expect_equal(3, qltriangle(0 + 0.1 + 0.1 + 0.1, 10^0, 10^10, 10^3))
+})
